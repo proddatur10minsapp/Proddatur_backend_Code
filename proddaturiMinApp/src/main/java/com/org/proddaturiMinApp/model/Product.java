2265c48d2ccc951e9d4178613fda27c1b2976e36
rupesh4950 <mekalarupesh7981842197@gmail.com>
2025-05-07 15:00:10 +0530

    pagination api

--- proddaturiMinApp/src/main/java/com/org/proddaturiMinApp/model/Product.java
+++ proddaturiMinApp/src/main/java/com/org/proddaturiMinApp/model/Product.java
@@ -3,6 +3,7 @@ package com.org.proddaturiMinApp.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -15,7 +16,7 @@ public class Product {
     @Id
     private String id;
     private String name;
-    private String category;
+    private ObjectId category;
     private String image;
     private int price;
     private int discountPrice;
--- proddaturiMinApp/src/main/java/com/org/proddaturiMinApp/service/impl/ProductServiceImpl.java
+++ proddaturiMinApp/src/main/java/com/org/proddaturiMinApp/service/impl/ProductServiceImpl.java
@@ -6,8 +6,15 @@ import com.org.proddaturiMinApp.repository.CategoryRepository;
 import com.org.proddaturiMinApp.repository.ProductRepository;
 import com.org.proddaturiMinApp.service.ProductService;
 import com.org.proddaturiMinApp.utils.CommonConstants;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,27 +31,10 @@ public class ProductServiceImpl implements ProductService {
     private CategoryRepository categoryRepository;
 
     public List<Product> getFilteredProducts(String categoryName, int i) {
-        List<Product> filteredProducts = new ArrayList<>();
-        List<Product> allProducts = productRepository.findAll();
+        Pageable pageable= PageRequest.of(i%10,10);
+        String id =categoryRepository.findByName(categoryName).get().getId();
+       return  productRepository.findByCategory(new ObjectId(categoryName),pageable);
 
-        if (i >= allProducts.size())
-            return filteredProducts;
-
-        while (i < allProducts.size()) {
-            Product product = allProducts.get(i);
-            if (Objects.isNull(product.getCategory()) ) {
-                i++;
-                continue; // skip if category is null
-            }
-
-            String productCategoryName = getCategoryNameById(product.getCategory());
-            if (productCategoryName.equalsIgnoreCase(categoryName)) {
-                if (filteredProducts.size() <= CommonConstants.paginationRange) filteredProducts.add(product);
-            }
-            i++;
-        }
-
-        return filteredProducts;
     }
 
     public List<Product> getProducts(String categoryName) {
