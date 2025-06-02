3b702439a9ef5013f592ea449da1c69e0497df52
Thillak <thillakkumar2002@gmail.com>
2025-06-03 05:29:39 +0530

    updates

--- proddaturiMinApp/src/main/java/com/org/proddaturiMinApp/controller/ProductFilterController.java
+++ proddaturiMinApp/src/main/java/com/org/proddaturiMinApp/controller/ProductFilterController.java
@@ -2,6 +2,7 @@ package com.org.proddaturiMinApp.controller;
 
 import com.org.proddaturiMinApp.dto.ProductFilterDTO;
 import com.org.proddaturiMinApp.service.ProductFilterService;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,14 +15,14 @@ public class ProductFilterController {
     @Autowired
     private ProductFilterService productFilterService;
 
-    @GetMapping("/{categoryName}/{methodology}/{technique}/{startIndex}")
+    @GetMapping("/{categoryId}/{methodology}/{technique}/{startIndex}")
     public List<ProductFilterDTO> filterProducts(
-            @PathVariable String categoryName,
+            @PathVariable ObjectId categoryId,
             @PathVariable String methodology,
             @PathVariable String technique,
             @PathVariable int startIndex,
             @RequestParam(value = "phone-number", required = false) String phoneNumber
     ) throws Exception {
-        return productFilterService.filterProducts(categoryName, methodology, technique, phoneNumber, startIndex);
+        return productFilterService.filterProducts(categoryId, methodology, technique, phoneNumber, startIndex);
     }
 }
--- proddaturiMinApp/src/main/java/com/org/proddaturiMinApp/service/ProductFilterService.java
+++ proddaturiMinApp/src/main/java/com/org/proddaturiMinApp/service/ProductFilterService.java
@@ -1,9 +1,10 @@
 package com.org.proddaturiMinApp.service;
 
 import com.org.proddaturiMinApp.dto.ProductFilterDTO;
+import org.bson.types.ObjectId;
 
 import java.util.List;
 
 public interface ProductFilterService {
-    List<ProductFilterDTO> filterProducts(String categoryName, String methodology, String technique, String phoneNumber, int startIndex) throws Exception;
+    List<ProductFilterDTO> filterProducts(ObjectId categoryId, String methodology, String technique, String phoneNumber, int startIndex) throws Exception;
 }
